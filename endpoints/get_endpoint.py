a53cebbbd9950a9ed0d391158da82de95eb2fae1
Sergey Popov <ser78910111@gmail.com>
2025-07-03 16:13:49 +0500

    Test Meme API project

--- endpoints/get_endpoint.py
+++ endpoints/get_endpoint.py
@@ -1,6 +1,6 @@
 import requests
 import allure
-from .base_endpoint import Endpoint
+from endpoints.base_endpoint import Endpoint
 
 
 class GetMeme(Endpoint):
@@ -8,14 +8,16 @@ class GetMeme(Endpoint):
     @allure.feature('GET request')
     @allure.story('Request to receive all memes')
     @allure.step('Получение всех объектов')
-    def get_meme(self, return_token):
+    def get_all_meme(self, return_token):
         self.response = requests.get(f'{self.url}/meme', headers=return_token)
         return self.response
 
     @allure.feature('GET request')
     @allure.story('Request meme by id')
     @allure.step('Запрос мема по id')
-    def get_meme_id(self, create_post, return_token):
-        self.response = requests.get(f'{self.url}/meme/{create_post['id']}', headers=return_token)
-        assert self.response.json()['id'] == create_post['id'], 'ID is incorrect'
+    def get_meme_id(self, create_meme, return_token):
+        self.response = requests.get(f'{self.url}/meme/{create_meme['id']}', headers=return_token)
         return self.response
+
+    def check_that_id_is_correct(self, create_meme):
+        assert self.response.json()['id'] == create_meme['id'], 'ID is incorrect'
--- endpoints/post_endpoint.py
+++ endpoints/post_endpoint.py
@@ -1,7 +1,7 @@
 import requests
 import allure
-from .base_endpoint import Endpoint
-from body_list import positive_body
+from endpoints.base_endpoint import Endpoint
+from utils.body_list import positive_body
 
 
 class PostMeme(Endpoint):
@@ -17,15 +17,15 @@ class PostMeme(Endpoint):
     @allure.feature('POST request')
     @allure.story('Checking that a meme has been created')
     @allure.step('Проверка, что мем создан')
-    def check_that_post_created(self, create_post, return_token):
-        self.response = requests.get(f'{self.url}/meme/{create_post['id']}', headers=return_token)
-        assert self.response.json()['id'] == create_post['id']
-        return self.response.json()['id']
+    def check_that_post_created(self, create_meme, return_token):
+        self.response = requests.get(f'{self.url}/meme/{create_meme['id']}', headers=return_token)
+        assert self.response.json() == create_meme
+        # return self.response.json()['id']
 
     @allure.feature('POST request')
     @allure.story('Checking the creation of a meme with invalid request body')
     @allure.step('Проверка создания мема с невалидным телом запроса')
-    def assert_post_invalid_body(self, status, body, return_token):
+    def check_post_meme_with_invalid_body(self, status, body, return_token):
         self.response = requests.post(f'{self.url}/meme', json=body, headers=return_token)
         if self.response.status_code != int(status):
             print(f'Bug: получил статус код {self.response.status_code} с данными {body}, вместо {status}')
